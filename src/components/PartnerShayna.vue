1868b89de9bc989dd3af1dc8fb0f4f8b001eb9af
LinggaWahyu <linggawahyurochim@gmail.com>
2020-06-06 08:28:58 +0700

    upload projek vue

---
+++ src/components/PartnerShayna.vue
@@ -0,0 +1,47 @@
+<template>
+    <!-- Partner Logo Section Begin -->
+    <div class="partner-logo">
+        <div class="container align-center">
+            <carousel class="logo-carousel" :loop="true" :items="5" :dots="false" :nav="false" :autoplay="true">
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-1.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-2.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-3.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-4.png" alt="" />
+                    </div>
+                </div>
+                <div class="logo-item">
+                    <div class="tablecell-inner">
+                        <img src="img/logo-carousel/logo-5.png" alt="" />
+                    </div>
+                </div>
+            </carousel>
+        </div>
+    </div>
+    <!-- Partner Logo Section End -->
+</template>
+
+<script>
+
+import carousel from 'vue-owl-carousel'
+
+export default {
+    name: 'WomenShayna',
+    components: {
+        carousel
+    }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenShayna from '@/components/WomenShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
